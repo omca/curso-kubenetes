daaf8bda2d1121be3277653254680abae18b1c0a
Omar Calderon <calderonomar48@gmail.com>
2023-02-10 00:23:59 -0500

    new message fot the last commmit, yeah

---
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/Animal.java
@@ -0,0 +1,9 @@
+package org.aguzman.springcloud.msvc.usuarios;/**
+ * Created by Omar Calderon Evangelista
+ * User: Omar.Calderon
+ * Date: 10/02/2023
+ * Time: 19:19
+ * Change Text
+*/
+public class Animal {
+}
